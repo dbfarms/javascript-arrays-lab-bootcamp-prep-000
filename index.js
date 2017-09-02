bc2741865a0c62ccfc424ced422cc395c3e33b1a
Matthew Hunger <matt.hunger@gmail.com>
2017-09-02 10:48:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(kittens) {
+  kittens.push("Ralph")
+}
+
+kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyPrependKitten(kittens) {
+  kittens.unshift("test")
+}
+
+kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyRemoveLastKitten(kittens) {
+  kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+}
